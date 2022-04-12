861b743f5cb5f00bd02f4bc6468bcaeeee99f5a2
rewq <8117523+rewq@users.noreply.github.com>
2022-04-11 23:28:10 -0400

    Update chocolateyinstall.ps1

--- automatic/burp-suite-pro-edition/tools/chocolateyinstall.ps1
+++ automatic/burp-suite-pro-edition/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'exe' 
   softwareName  = 'Burp*' 
 
-  url64bit      = 'https://portswigger-cdn.net/burp/releases/download?product=pro&version=2022.3.3&type=WindowsX64'
+  url64bit      = 'https://portswigger.net/burp/releases/startdownload?product=pro&version=2022.3.3&type=WindowsX64'
   checksumType64= 'sha256' 
   checksum64    = 'ec36e4e087bca78950b27d33f97ab27102aee57ea66bd4540eb07ea7d4ea3934'
 
