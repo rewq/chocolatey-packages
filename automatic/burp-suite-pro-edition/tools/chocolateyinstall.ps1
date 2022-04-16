a56d1543df74f61ae6b57a2552857041ebec5120
rewq <8117523+rewq@users.noreply.github.com>
2022-04-15 21:19:43 -0400

    Update chocolateyinstall.ps1

--- automatic/burp-suite-pro-edition/tools/chocolateyinstall.ps1
+++ automatic/burp-suite-pro-edition/tools/chocolateyinstall.ps1
@@ -13,6 +13,7 @@ $packageArgs = @{
   checksum64    = 'ec36e4e087bca78950b27d33f97ab27102aee57ea66bd4540eb07ea7d4ea3934'
 
   silentArgs   = '-q'           # Install4j
+  validExitCodes= @(0)
 }
 
 Install-ChocolateyPackage @packageArgs 
